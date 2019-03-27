53dd7ae2f0bbf6cbd901a9136a3fe55160b23261
Molly Nicholas <molecule@berkeley.edu>
2019-03-27 14:20:50 -0700

    a simple mic recording app that works on iphone

---
+++ Unity Mic Check/Assets/RecordAudio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordAudio : MonoBehaviour
+{
+    
+    public AudioSource audioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var device in Microphone.devices) {
+            Debug.Log("====== Name: " + device);
+        }
+        
+    }
+
+    // Update is called once per frameu
+    void Update()
+    {
+        
+    }
+
+    public void Record() {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Microphone.Start("iPhone audio input", false, 10, 44100);
+    }
+
+    public void Play() {
+        audioSource.Play();
+    }
+}
